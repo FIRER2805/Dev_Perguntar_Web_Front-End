6af40318a4f0395f5a044fde823620a8a774114d
Gabriel Henrique Muller de Medeiros <gabriel_henrique2805@hotmail.com>
2023-11-30 18:44:05 -0300

    Home dinamica
    Tela de home pegando as perguntas da api e mostrando na tela de forma dinamica

--- src/app/pergunta/listar-perguntas/listar-perguntas.component.ts
+++ src/app/pergunta/listar-perguntas/listar-perguntas.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Pergunta } from 'src/app/shared/model/entity/Pergunta';
 import { PerguntaService } from 'src/app/shared/service/pergunta-service.service';
 
 @Component({
@@ -8,14 +9,15 @@ import { PerguntaService } from 'src/app/shared/service/pergunta-service.service
   styleUrls: ['./listar-perguntas.component.scss']
 })
 export class ListarPerguntasComponent implements OnInit {
-  
+
+  perguntas: Pergunta[] = [];
+
   constructor(private service: PerguntaService){}
-  
+
   ngOnInit(): void {
     this.service.buscaTodos().subscribe(
       resultado => {
-        //TODO fazer algo com o resultado
-        console.log(resultado);
+        this.perguntas = resultado;
       },
       erro => {
         //TODO usar Swal.
